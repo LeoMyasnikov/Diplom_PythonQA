e41f97165222782380daa68dc069ed6f6710dd98
qahelping <122584520+qahelping@users.noreply.github.com>
2025-01-14 20:43:34 +0300

    Update test_scanning.py

--- tests/test_scanning.py
+++ tests/test_scanning.py
@@ -7,7 +7,8 @@ class TestScanningPage:
     def test_open_scanning_page(self, driver):
         login_page = loginPage(driver)
         login_page.open_scanning()
-        scanning_page = login_page.login_in_scanning()
+
+        scanning_page = ScaningPage(driver)
         scanning_page.assert_scanning_page_is_opened()
 
     def test_project_choice(self, driver):

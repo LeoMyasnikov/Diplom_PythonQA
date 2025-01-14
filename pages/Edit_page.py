b09955ba2df367f239e1cfb0bd3c7ddbfc058fe1
qahelping <122584520+qahelping@users.noreply.github.com>
2025-01-14 20:26:01 +0300

    Update Edit_page.py

--- pages/Edit_page.py
+++ pages/Edit_page.py
@@ -36,6 +36,7 @@ class EditPage(Base):
 
     @allure.step('Проверка на открытие страницы редактирования данных входа')
     def assert_edit_page_is_opened(self):
+        # сделать отдельную функции и ее вызывать
         element = self.get_element(self.edit_page)
         assert element.is_displayed(), f"Element {self.edit_page} is not visible"
 
@@ -54,12 +55,14 @@ class EditPage(Base):
 
         self.click_on(self.save_button)
 
+        # отдельная функция get_element (проверять что виден)
         error_message_element = WebDriverWait(self.driver, 5).until(
             EC.visibility_of_element_located(self.error_message_simple_password))
 
         # Выводим фактический текст ошибки для отладки
         actual_error_message = error_message_element.text.strip().replace('\n', ' ')
-        print(f"Фактическое сообщение об ошибке: '{actual_error_message}'")
+        
+        # print(f"Фактическое сообщение об ошибке: '{actual_error_message}'")
 
         # Проверка текста ошибки
         expected_error_message = ("Ваш пароль не может быть менее 8 символов. " 
@@ -113,6 +116,9 @@ class EditPage(Base):
         actual_error_message = error_message_element.text.strip()
         print(f"Фактическое сообщение об ошибке: '{actual_error_message}'")
 
+        # поместить в отдельный степ
+
+    def assert___()
         expected_error_message = 'Это поле должно быть заполнено'
         assert actual_error_message == expected_error_message, (
             f"Expected: '{expected_error_message}', but got: '{actual_error_message}'")
